02116a94593b7b1cda03d7b3e242fc092ad50747
mobinhassani9374 <mobinhassani9374@gmail.com>
2019-04-21 00:27:55 +0430

    db init

---
+++ ReactNetCore/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ReactNetCore.Data.Entities;
+
+namespace ReactNetCore.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
--- ReactNetCore/Startup.cs
+++ ReactNetCore/Startup.cs
@@ -5,6 +5,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace ReactNetCore
 {
@@ -20,6 +22,11 @@ namespace ReactNetCore
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<Data.AppDbContext>(options =>
+            {
+                options.UseSqlServer(@"Data Source=185.51.200.186\SQL2014,2014;Initial Catalog=ferdows5_ReactNetCore;Persist Security Info=True;User ID=mobin; Password=#q9y6f3Y; MultipleActiveResultSets=True");
+            });
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             // In production, the React files will be served from this directory
@@ -30,8 +37,10 @@ namespace ReactNetCore
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, Data.AppDbContext db)
         {
+            db.Database.Migrate();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
