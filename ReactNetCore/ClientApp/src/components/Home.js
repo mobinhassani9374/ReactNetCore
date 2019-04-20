41eee549a2ae543a1de549103bb30f3d7d45be11
mahdihassani9374 <mahdihassani9374@gmail.com>
2019-04-20 08:31:10 +0430

    add axios

--- ReactNetCore/ClientApp/src/components/Home.js
+++ ReactNetCore/ClientApp/src/components/Home.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+import Request from '../Service/Request';
 
 export class Home extends Component {
 
   render() {
+    console.log(Request('https://jsonplaceholder.typicode.com/users'))
     return (
       <div>
         <h1>Hello, world!</h1>‌ 
