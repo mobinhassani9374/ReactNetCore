2a65867d222c40111c5b126da35b651022fad708
mobinhassani9374 <mobinhassani9374@gmail.com>
2019-04-26 21:31:08 +0430

    add AccountController and LOginModel and LoginResponseModel

---
+++ ReactNetCore/Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity.UI.Pages.Account.Internal;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ReactNetCore.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpPost]
+        [AllowAnonymous()]
+        public IActionResult Login([FromBody]LoginModel model)
+        {
+            return Ok();
+        }
+    }
+}
