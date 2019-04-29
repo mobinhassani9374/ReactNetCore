eca733a1b1d9bfde03bb1fe468386d9aa3b64753
mobinhassani <mobinhassani9374@gmail.com>
2019-04-29 17:09:12 +0430

    develop of RoutineController

--- ReactNetCore/Controllers/RoutineController.cs
+++ ReactNetCore/Controllers/RoutineController.cs
@@ -3,11 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ReactNetCore.RoutineBuilder;
 
 namespace ReactNetCore.Controllers
 {
     public class RoutineController : Controller
     {
-        
+        private readonly RoutineRepository _routineRepository;
+
+        public RoutineController(RoutineRepository routineRepository)
+        {
+            _routineRepository = routineRepository;
+        }
+
+        [Route("api/routine/getrolesWithdashboardcreation/{routineId}")]
+        [HttpGet]
+        public IActionResult GetRoleWithDashboardCreation(int routineId)
+        {
+            var roles = _routineRepository.GetRoles(routineId);
+
+            return Ok();
+        }
     }
 }
