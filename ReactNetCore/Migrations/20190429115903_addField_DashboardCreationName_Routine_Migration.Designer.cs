fa137a8b57d21d501f237193f6068edefe15516b
mobinhassani <mobinhassani9374@gmail.com>
2019-04-29 16:29:15 +0430

    addField_DashboardCreationName_Routine_Migration

--- ReactNetCore/Migrations/20190429115903_addField_DashboardCreationName_Routine_Migration.Designer.cs
+++ ReactNetCore/Migrations/20190429115903_addField_DashboardCreationName_Routine_Migration.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ReactNetCore.Data;
 
 namespace ReactNetCore.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190429115903_addField_DashboardCreationName_Routine_Migration")]
+    partial class addField_DashboardCreationName_Routine_Migration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
