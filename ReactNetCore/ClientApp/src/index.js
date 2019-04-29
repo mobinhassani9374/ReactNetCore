7c4218951667bb9bc1fd31b07c01ba0521053282
mahdihassani9374 <mahdihassani9374@gmail.com>
2019-04-29 14:52:10 +0430

    add redux

--- ReactNetCore/ClientApp/src/index.js
+++ ReactNetCore/ClientApp/src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { ConnectedRouter } from 'react-router-redux';
+import { createBrowserHistory } from 'history';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
 import 'bootstrap-4.1.3-rtl/css/bootstrap.css';
 import 'axios-progress-bar/dist/nprogress.css'
 import '../node_modules/font-awesome/css/font-awesome.css';
@@ -11,14 +15,32 @@ import './docs/css/animate.css'
 import './docs/css/styles.css?v=0.0.1'
 import './docs/lib/pe-icon-7-stroke/css/helper.css'
 import './docs/lib/pe-icon-7-stroke/css/pe-icon-7-stroke.css'
+import { stat } from 'fs';
 
+// Create browser history to use in the Redux store
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
+const history = createBrowserHistory({ basename: baseUrl });
+
+// Get the application-wide store instance, prepopulating with state from the server where available.
+const initialState = window.initialReduxState;
+
+let reducer =()=>{
+  return {
+
+  }
+}
+
+const store = createStore(reducer)
+
 const rootElement = document.getElementById('root');
 
 ReactDOM.render(
-  <BrowserRouter basename={baseUrl}>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <App />
+    </ConnectedRouter>
+  </Provider>,
   rootElement);
 
 registerServiceWorker();
+
