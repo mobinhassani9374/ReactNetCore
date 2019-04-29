2a5114b36d401b235bae029cddb0a324db32f84b
mobinhassani <mobinhassani9374@gmail.com>
2019-04-29 11:28:13 +0430

    AddEntities_RoutineBuilder_Migration

--- ReactNetCore/Migrations/20190429065659_AddEntities_RoutineBuilder_Migration.Designer.cs
+++ ReactNetCore/Migrations/20190429065659_AddEntities_RoutineBuilder_Migration.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ReactNetCore.Data;
 
 namespace ReactNetCore.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190429065659_AddEntities_RoutineBuilder_Migration")]
+    partial class AddEntities_RoutineBuilder_Migration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
