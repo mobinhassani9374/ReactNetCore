c40825430a8ce8266bf7d3efbc8f667321550bf9
mobinhassani <mobinhassani9374@gmail.com>
2019-04-29 11:46:00 +0430

    add RoutineSearchCriteria

---
+++ ReactNetCore/RoutineBuilder/RoutineSearchCriteria.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReactNetCore.RoutineBuilder
+{
+    public abstract class RoutineSearchCriteria
+    {
+        public DashboardTypes DashboardType { get; set; }
+
+        public int UserId { get; set; }
+    }
+}
