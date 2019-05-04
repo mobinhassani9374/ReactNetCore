cd8f0d5ce1cd27071cb3122164b2331293c26fd1
mahdihassani9374 <mahdihassani9374@gmail.com>
2019-05-04 15:26:44 +0430

    layout menu responsive

--- ReactNetCore/ClientApp/src/components/Layout.js
+++ ReactNetCore/ClientApp/src/components/Layout.js
@@ -27,59 +27,65 @@ class Layout extends Component {
     return (      
       <div>
         <nav className="navbar navbar-expand-sm bg-dark navbar-dark sticky-top">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <NavLink className="nav-link" exact={true} to="/">صفحه اصلی</NavLink>
-            </li>
-            {
-              authorization ? 
-                <li className="nav-item">
-                  <NavLink className="nav-link" to="/users">لیست کاربران</NavLink>
-                </li> : ''
-            }           
-            {
-              authorization ? 
-                <li className="nav-item">
-                  <NavLink className="nav-link" to="/users-add">اضافه نمودن کاربر جدید </NavLink>
-                </li>  : ''
-            }   
-            {
-              authorization ? 
-                <li className="nav-item">
-                  <NavLink className="nav-link" to="/cartable/1">روال اول</NavLink>
-                </li> : ''
-            }                                                                  
-            {
-              authorization ? 
-                <li className="nav-item">
-                  <NavLink className="nav-link" to="/cartable/2">روال دوم</NavLink>
-                </li> : ''
-            }                                                                  
-            {
-              authorization ? 
-                <li className="nav-item">
-                  <NavLink className="nav-link" to="/cartable/3">روال سوم</NavLink>
-                </li> : ''
-            }                                                                  
-            {
-              authorization ? 
-                <li className="nav-item">
-                  <NavLink className="nav-link" to="/cartable/4">روال چهارم</NavLink>
-                </li> : ''
-            }                                                                  
-            {
-              authorization ? 
-                <li className="nav-item">
-                    <a onClick={this.logout} className="nav-link" >خروج</a>
-                </li>  : ''
-            }
-            {
-              !authorization ? 
-                <li className="nav-item">
-                  <NavLink className="nav-link" to="/login">ورود</NavLink>
-              </li> : ''              
-            }                                                                      
-          </ul>
+          {/* <NavLink to="/" class="navbar-brand" href="#">React.NteCore</NavLink> */}
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <NavLink className="nav-link" exact={true} to="/">صفحه اصلی</NavLink>
+              </li>
+              {
+                authorization ? 
+                  <li className="nav-item">
+                    <NavLink className="nav-link" to="/users">لیست کاربران</NavLink>
+                  </li> : ''
+              }           
+              {
+                authorization ? 
+                  <li className="nav-item">
+                    <NavLink className="nav-link" to="/users-add">اضافه نمودن کاربر جدید </NavLink>
+                  </li>  : ''
+              }   
+              {
+                authorization ? 
+                  <li className="nav-item">
+                    <NavLink className="nav-link" to="/cartable/1">روال اول</NavLink>
+                  </li> : ''
+              }                                                                  
+              {
+                authorization ? 
+                  <li className="nav-item">
+                    <NavLink className="nav-link" to="/cartable/2">روال دوم</NavLink>
+                  </li> : ''
+              }                                                                  
+              {
+                authorization ? 
+                  <li className="nav-item">
+                    <NavLink className="nav-link" to="/cartable/3">روال سوم</NavLink>
+                  </li> : ''
+              }                                                                  
+              {
+                authorization ? 
+                  <li className="nav-item">
+                    <NavLink className="nav-link" to="/cartable/4">روال چهارم</NavLink>
+                  </li> : ''
+              }                                                                  
+              {
+                authorization ? 
+                  <li className="nav-item">
+                      <a onClick={this.logout} className="nav-link" >خروج</a>
+                  </li>  : ''
+              }
+              {
+                !authorization ? 
+                  <li className="nav-item">
+                    <NavLink className="nav-link" to="/login">ورود</NavLink>
+                </li> : ''              
+              }                                                                      
+            </ul>
+          </div>          
         </nav>                    
       </div>
     )
