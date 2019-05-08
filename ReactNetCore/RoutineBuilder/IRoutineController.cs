378630d7f5a6817786aad9fd89877df5d597715b
mobinhassani <mobinhassani9374@gmail.com>
2019-05-08 15:30:43 +0430

    add IRoutineController

---
+++ ReactNetCore/RoutineBuilder/IRoutineController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReactNetCore.RoutineBuilder
+{
+    public interface IRoutineController
+    {
+        IActionResult Manage<TSearchCritria>(TSearchCritria searchModel) where TSearchCritria : RoutineSearchCriteria;
+    }
+}
