033c9a0dda626b098bd640cfb26e15892aaf4b6d
mobinhassani <mobinhassani9374@gmail.com>
2019-05-08 15:58:09 +0430

    develop of RegisterModule

---
+++ ReactNetCore/Controllers/RegisterModuleController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ReactNetCore.Models;
+using ReactNetCore.RoutineBuilder;
+
+namespace ReactNetCore.Controllers
+{
+    public class RegisterModuleController : Controller
+    {
+        [HttpPost]
+        [Route("api/RegisterModule/manage")]
+        public IActionResult Manage([FromBody]RegisterModuleSearchCritria searchModel)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ ReactNetCore/Models/RegisterModuleSearchCritria.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReactNetCore.Models
+{
+    public class RegisterModuleSearchCritria : RoutineBuilder.RoutineSearchCriteria
+    {
+    }
+}
