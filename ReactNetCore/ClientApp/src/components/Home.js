fed4523c7b59a190e8a59879ce95562f1b515e88
mahdihassani9374 <mahdihassani9374@gmail.com>
2019-05-11 13:29:39 +0430

    add sweet alert

--- ReactNetCore/ClientApp/src/components/Home.js
+++ ReactNetCore/ClientApp/src/components/Home.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 import Layout from './Layout';
 import { connect } from 'react-redux';
+
+import SweetAlert from 'react-bootstrap-sweetalert'
 class Home extends Component {
+  state = {
+    show:true
+  }
 
   render() {   
     return (
       <div>
         <Layout />
+        {
+          this.state.show ? 
+          <SweetAlert  success title="پنل ادمین " onConfirm={()=>{this.setState({show:false})}}>
+            خوش آمدید !!!!
+          </SweetAlert> : ''
+        }
       </div>
     );
   }
