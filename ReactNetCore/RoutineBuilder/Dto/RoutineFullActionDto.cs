5eff8c5a146dd3a59caa9bee0ef734a2bd8c9434
mobinhassani <mobinhassani9374@gmail.com>
2019-05-14 11:56:01 +0430

    add RoutineFullActionDto

---
+++ ReactNetCore/RoutineBuilder/Dto/RoutineFullActionDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReactNetCore.RoutineBuilder.Dto
+{
+    public class RoutineFullActionDto
+    {
+        public string Title { get; set; }
+
+        public string Icon { get; set; }
+
+        public string Color { get; set; }
+
+        public bool IsCustomAction { get; set; }
+
+        public string ComponentName { get; set; }
+
+        public string RelativePath { get; set; }
+    }
+}
