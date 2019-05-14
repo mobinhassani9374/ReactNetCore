d8e36947d17c962d1fbfa98c7fe9fab559a2503a
mobinhassani <mobinhassani9374@gmail.com>
2019-05-14 12:10:38 +0430

    add ActionFactory

---
+++ ReactNetCore/RoutineBuilder/ActionFactory.cs
@@ -0,0 +1,21 @@
+﻿using ReactNetCore.RoutineBuilder.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReactNetCore.RoutineBuilder
+{
+    public class ActionFactory<TDto> where TDto : RoutineDto
+    {
+        public static List<TDto> Build(List<TDto> data, DashboardTypes dashboardType, List<RoutineActionSummaryDto> actions, List<RoutineCustomActionSummaryDto> customActions, RoutineSummaryDto routine)
+        {
+            data.ForEach(c =>
+            {
+                
+            });
+
+            return data;
+        }
+    }
+}
