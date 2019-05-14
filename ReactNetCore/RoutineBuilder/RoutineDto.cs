abb88fdee9ad9b4b01a17a88b8b219073c75e192
mobinhassani <mobinhassani9374@gmail.com>
2019-05-14 11:58:33 +0430

    develop of RoutineDto

---
+++ ReactNetCore/RoutineBuilder/RoutineDto.cs
@@ -0,0 +1,13 @@
+﻿using ReactNetCore.RoutineBuilder.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReactNetCore.RoutineBuilder
+{
+    public class RoutineDto : RoutineEntity
+    {
+        public List<RoutineFullActionDto> Actions { get; set; }
+    }
+}
