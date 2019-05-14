87fa85a148800652f7a67da34d873b613edc32ab
mobinhassani <mobinhassani9374@gmail.com>
2019-05-14 11:50:14 +0430

    add RoutineCustomActionSummaryDto

---
+++ ReactNetCore/RoutineBuilder/Dto/RoutineActionSummaryDto.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReactNetCore.RoutineBuilder.Dto
+{
+    public class RoutineActionSummaryDto
+    {
+        public int Id { get; set; }
+
+        /// <summary>
+        /// شماره مرحله
+        /// </summary>
+        public int Step { get; set; }
+
+
+        /// <summary>
+        /// عملیاتی که انجام شده است
+        /// F1, ... F7
+        /// </summary>
+        public string Action { get; set; }
+
+
+        /// <summary>
+        /// عنوان عملیات نظیر ارسال یا تایید
+        /// </summary>
+        public string Title { get; set; }
+
+        public string Icon { get; set; }
+
+        public string Color { get; set; }
+
+
+
+        public int RoutineId { get; set; }
+
+        public virtual RoutineSummaryDto Routine { get; set; }
+    }
+}
---
+++ ReactNetCore/RoutineBuilder/Dto/RoutineCustomActionSummaryDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReactNetCore.RoutineBuilder.Dto
+{
+    public class RoutineCustomActionSummaryDto
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Icon { get; set; }
+
+        public string Color { get; set; }
+
+        public DashboardTypes DashboardType { get; set; }
+
+        public int? Step { get; set; }
+
+        public string RemoveAction { get; set; }
+
+        public virtual RoutineSummaryDto Routine { get; set; }
+
+        public int RoutineId { get; set; }
+    }
+}
